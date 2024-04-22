39135fdaa7ed73844b4f1c6380c90f79ab0f49c2
User Podsekina <margaritapodsekina@gmail.com>
2024-04-23 01:19:15 +0300

    Исправила ошибки

--- src/test/InMemoryTaskManagerTest.java
+++ src/test/InMemoryTaskManagerTest.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class InMemoryTaskManagerTests {
+public class InMemoryTaskManagerTest {
     private TaskManager taskManager;
 
     @BeforeEach
