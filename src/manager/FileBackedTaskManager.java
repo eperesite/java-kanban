236aa284188636da625fe30ae6849eba543f99ff
User Podsekina <margaritapodsekina@gmail.com>
2024-05-06 22:01:27 +0300

    4ver

--- src/manager/FileBackedTaskManager.java
+++ src/manager/FileBackedTaskManager.java
@@ -193,7 +193,7 @@ public class FileBackedTaskManager extends InMemoryTaskManager {
     @Override
     public void deleteEpic(Integer idNumber) {
         super.deleteEpic(idNumber);
-        save();
+        save(); 
     }
 
     @Override
